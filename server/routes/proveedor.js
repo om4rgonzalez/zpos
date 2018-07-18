c7f6909b0ed2d368a596c5a6965651e14bed84e4
Marcelo <om4r.gonzalez@gmail.com>
2018-07-18 11:11:37 -0300

    se agrego un metodo para devovler una lista de todos los proveedores

--- server/routes/proveedor.js
+++ server/routes/proveedor.js
@@ -3,7 +3,7 @@ const app = express();
 
 const Proveedor = require('../models/proveedor');
 
-app.post('/producto/inicializar_proveedores', function(req, res) {
+app.post('/config/inicializar_proveedores', function(req, res) {
 
     let valores = [{ nombre: 'BUEN DIA', cuit: '00000000000' }];
 
@@ -18,11 +18,44 @@ app.post('/producto/inicializar_proveedores', function(req, res) {
 
         item.save();
     }
+})
 
+app.get('/proveedores/todos', async function(req, res) {
+    // let comercio = req.query.comercio;
+    // console.log(`El proveedor a buscar es ${proveedor}`);
+    Proveedor.find()
+        .exec((err, proveedores) => {
 
+            // console.log(usuarios.length);
 
+            if (err) {
+                return res.status(500).json({
+                    ok: false,
+                    err
+                });
+            }
 
+            if (!proveedores) {
+                return res.status(400).json({
+                    ok: false,
+                    err: {
+                        message: 'No hay proveedores'
+                    }
+                });
+            }
 
+            // clientes = clientes.filter(function(clientes) {
+            //     return clientes.titular != null;
+            // })
+
+            return res.json({
+                ok: true,
+                recordsTotal: proveedores.length,
+                // recordsFiltered: clientes.length,
+                proveedores
+            });
+
+        });
 })
 
 
